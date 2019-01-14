496024a135eba49fb10bb30f1d7c630db02c22df
Andranique Boone <andranique.boone@gmail.com>
2019-01-14 00:49:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 # Your code goes here!
 class Dog 
-  def initialize (name)
+  def initialize (name, bark="woof!")
     @name = name 
+    @bark = "woof!"
   end 
   def name
     @name
   end 
-  def name=(new_Dog)
-    @name = new_Dog
-  end
   def bark
     puts "woof!"
   end 
+  def name=(new_Dog)
+    @name = new_Dog
+  end
 end 
 
 kingsley = Dog.new 
 kingsley.name = "Kingsley"
-kingsley.name
 kingsley.bark
